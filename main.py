9c30df5217149f4d31e68baa6a5259231090fbb2
satwinder kaur <ksatwinder865@gmail.com>
2025-07-25 13:52:47 +0530

    added content

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-
-
 # from typing import Union
 from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/about")
-def about_item():
+def about():
     return {"hey":"cutie"}
 
+@app.get('/blog/{id}')
+def about_item(id):
+    return {"data":"id"}
 
 
