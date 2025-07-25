ae0f71f383227dc1cb5e76f1e3e56fa3aff111d7
satwinder kaur <ksatwinder865@gmail.com>
2025-07-25 12:49:23 +0530

    fastapi

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 
 
+# from typing import Union
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/about")
+def about_item():
+    return {"hey":"cutie"}
+
 
 
